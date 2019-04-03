89f0ebf2a0e1caac915d11b2cfe8c96fa59067ea
João Dinis <j.p.dinis89@gmail.com>
2019-04-03 09:19:27 +0100

    Initial Commit - Time Taken:6h Contents:  - Researched and Selected type of app  - Started implementing Material Design UI  - Prepared Bottom Navigation Bar for a music player   - Allows active menu item highlighting according to design guidelines   - Keeps showing text contrary to what design guidelines specify to avoid mystery items  - Added icons for Bottom Navigation Bar  - Added logo to the title bar  - Opted for dark colored theme  - Dealt with some warnings  - Added strings to resources
    Signed-off-by: João Dinis <j.p.dinis89@gmail.com>

--- app/src/main/java/pt/jdinis/musicplayer/MainActivity.java
+++ app/src/main/java/pt/jdinis/musicplayer/MainActivity.java
@@ -1,13 +1,23 @@
 package pt.jdinis.musicplayer;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+            getSupportActionBar().setDisplayUseLogoEnabled(true);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to display logo on title bar.");
+        }
     }
 }
